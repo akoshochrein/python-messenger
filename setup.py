2af2be6a5dee6ae2b377c9e277542360ac068f3e
akoskaaa <akos.hochrein@prezi.com>
2016-11-13 22:18:28 +0100

    pypi release information

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='python-messenger',
+    packages=['python-messenger'],
+    version='0.1',
+    description='Bot class for Facebook Messenger',
+    author='Akos Hochrein',
+    author_email='hoch.akos@gmail.com',
+    url='https://github.com/akoskaaa/python-messenger',
+    download_url='https://github.com/akoskaaa/python-messenger/tarball/0.1',
+    keywords=['messenger', 'chat', 'bot', 'chatbot'],
+    classifiers=[]
+)

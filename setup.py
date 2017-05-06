2d7f6d123296403e64195a4a3e11cb4056e246f5
akoskaaa <akos.hochrein@prezi.com>
2017-05-06 14:54:12 +0200

    improve distribution

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='python-messenger',
     packages=['messenger'],
-    version='0.1',
+    version='0.1.1',
     description='Bot class for Facebook Messenger',
     author='Akos Hochrein',
     author_email='hoch.akos@gmail.com',
     url='https://github.com/akoskaaa/python-messenger',
     download_url='https://github.com/akoskaaa/python-messenger/tarball/0.1',
     keywords=['messenger', 'chat', 'bot', 'chatbot'],
-    classifiers=[]
+    classifiers=[],
+    license='Apache License 2.0',
 )
